32116a1851ef135c8a529faa72acd9597c413c08
Bigcujo <princefrancisogbogu@gmail.com>
2024-01-18 08:19:07 -0800

    hello

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for first in range(0, 10):
+    for second in range(first + 1, 10):
+        if first < 9 and second <= 9:
+            print("{:d}{:d}".format(first, second), end= ", ")
