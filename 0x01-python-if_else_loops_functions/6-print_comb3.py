4bdc66708f73a691288b14c068300d3b28d5c9d2
Bigcujo <princefrancisogbogu@gmail.com>
2024-01-18 08:27:53 -0800

    hello

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for first in range(0, 10):
     for second in range(first + 1, 10):
-        if first < 9 and second <= 9:
+        if first == 8 and second == 9:
+            print(89)
+        elif first < 9 and second <= 9:
             print("{:d}{:d}".format(first, second), end= ", ")
