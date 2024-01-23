4e0486230f0973267bcdccc8fd38e8a199b1ef3b
Bigcujo <princefrancisogbogu@gmail.com>
2024-01-23 15:09:26 -0800

    hello

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matris=[[]]):
+    if not matris:
+        print()
+    else:
+        for rows in range(len(matris)):
+            for item in range(len(matris[rows])):
+                if item != len(matrixs[rows]) - 1:
+                    endspace = ' '
+                else:
+                    endspace = ''
+                print("{:d}".format(matris[rows][item]), end=endspace)
+            print()
