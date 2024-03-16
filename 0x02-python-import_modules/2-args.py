736d4b016388df0823f1f805cc57366a841d98e7
bigcujo <princefrancisogbogu@gmail.com>
2024-03-16 00:58:58 -0700

    hello

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-argc = len(argv) - 1
-plural = 's' if argc != 1 else ''
-colon = ':' if argc != 0 else '.'
+    argc = len(argv) - 1
+    plural = 's' if argc != 1 else ''
+    colon = ':' if argc != 0 else '.'
 
-print("{} argument{}{}{}".format(argc, plural, colon, '\n' if argc else ''))
+    print("{} argument{}{}{}".format(argc, plural, colon, '\n' if argc else ''))
 
-for i in range(1, argc + 1):
+    for i in range(1, argc + 1):
         print("{}: {}".format(i, argv[i]))
