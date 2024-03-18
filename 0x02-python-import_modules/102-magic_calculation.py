716c6fa813f6d29a888a455d9f0281c70349972a
bigcujo <princefrancisogbogu@gmail.com>
2024-03-18 06:32:36 -0700

    hello

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+def magic_calculation(a, b):
+    add = __import__('magic_calculation_102', globals(), locals(), ['add'], 0).__dict__['add']
+    sub = __import__('magic_calculation_102', globals(), locals(), ['sub'], 0).__dict__['sub']
+    
+    if a < b:
+        c = add(a, b)
+        for x in range(4, 6):
+            c = add(c, x)
+        return c
+    else:
+        return sub(a, b)
