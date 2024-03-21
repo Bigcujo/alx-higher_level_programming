bd5e3082022b568adbf776b30c0c1cb4c809a32f
bigcujo <princefrancisogbogu@gmail.com>
2024-03-21 14:08:34 -0700

    hello

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    #creating an empty matrix to store the answer
+    result_matrix = []
+
+    #nested for loop
+    for rows in matrix:
+        #create a new row for new matrix
+        result_rows = []
+
+        for elements in row:
+            result_rows.append(elements ** 2)
+        result_matrix.append(result_rows)
+    return result_matrix    
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+this will be for more data structure 
