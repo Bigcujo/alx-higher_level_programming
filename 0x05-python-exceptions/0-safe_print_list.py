f38461b77b3c62dacbbd425e0547df11619b9d80
bigcujo <princefrancisogbogu@gmail.com>
2024-04-03 07:49:06 -0700

    hello

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x):
 
     num = 0
 
@@ -13,4 +13,3 @@ def safe_print_list(my_list=[], x=0):
             break
         print("")
         return num
-
