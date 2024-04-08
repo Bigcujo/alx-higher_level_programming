2e634bc15773a78f62682a1b6b8b50eab5c22fcb
bigcujo <princefrancisogbogu@gmail.com>
2024-04-08 08:50:44 -0700

    hello

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/pyhton3
+
+"""State a class Square"""
+
+class Square:
+    """this be for the square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+        size (int) : the size of the new square.
+
+        """
+        self.size = size
+
+        @get.size
+
+    def size(self):
+            """ Get the current size of the square"""
+            return (self.__size)
+
+        @size.setter
+    
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError ("size must be an integer")
+        elif value < 0:
+            raise ValueError ("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """ Return the area of the square"""
+        return (self.__size ** 2)
