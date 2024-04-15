4fa3f433203e5c9eee702391e1f05026926ae16c
bigcujo <princefrancisogbogu@gmail.com>
2024-04-15 08:50:58 -0700

    hello

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module for BaseGeometry class."""
+
+class BaseGeometry:
+    def area(self):
+        """Raises Exception error with the message "area() is not implemented"."""
+        raise Exception("area() is not implemented")
