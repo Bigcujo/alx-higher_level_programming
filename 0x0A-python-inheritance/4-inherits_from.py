616dfd418a50e83193ad824c0f4e277d5748ae7a
bigcujo <princefrancisogbogu@gmail.com>
2024-04-15 08:27:49 -0700

    hello

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-'''Module for is_kind_of_class method.'''
+'''Module for inherits_from  method.'''
 
 
 def inherits_from(obj, a_class):
     '''Check if the object is an instance of a class inherited (directly or indirectly) from the specified class.'''
     return isinstance(obj, object) and issubclass(type(obj), a_class) and type(obj) != a_class
-
