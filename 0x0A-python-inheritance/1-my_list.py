2a42efc202de4a5329954b7a780005e60dea363b
bigcujo <princefrancisogbogu@gmail.com>
2024-04-15 07:53:02 -0700

    hello

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module for Mylist class with print_sorted as method.'''
+
+
+class MyList(list):
+    '''my local MyList class.'''
+    def print_sorted(self):
+        '''Method with the sorted function to print out the sorted list.'''
+        print(sorted(self))
