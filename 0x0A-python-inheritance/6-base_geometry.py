9938ca0b27d474ae1e01231d40fda6f3b7977950
bigcujo <princefrancisogbogu@gmail.com>
2024-04-16 10:56:44 -0700

    hello

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -2,6 +2,7 @@
 """ Module for BaseGeometry class."""
 
 class BaseGeometry:
+    """this class will return the area. """
     def area(self):
         '''Raises Exception error with the message "area() is not implemented".'''
         raise Exception("area() is not implemented")
