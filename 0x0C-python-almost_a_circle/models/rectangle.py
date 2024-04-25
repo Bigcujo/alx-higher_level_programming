385e0418558d30bd32b98720284076834e900218
bigcujo <princefrancisogbogu@gmail.com>
2024-04-25 09:52:16 -0700

    new update on comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,9 +5,14 @@ this will be for the rectangele class.
 from models.base import Base
 
 class Rectangle(Base):
+   """
+   Rectangle class
+   """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-
+        """
+        initialize the new rectangle.
+        """
         self.width = width
         self.height = height
         self.x = x
@@ -16,34 +21,58 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        this is the getter method.
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        this is the setter method.
+        """
         self.__width = value
 
 
     @property
     def height(self):
+        """
+        this is the getter method.
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        this is the setter method
+        """
         self.__height = value
 
 
     @property
     def x(self):
+        """
+        this is the getter method
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        this is the setter method
+        """
         self.__x = value
 
     @property
     def y(self):
+        """
+        this is the getter method
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        this is the setter method.
+        """
         self.__y = value
