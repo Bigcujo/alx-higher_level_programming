70279753b55f61e1d2c096460cf0459d81b4c05f
bigcujo <princefrancisogbogu@gmail.com>
2024-04-26 10:35:45 -0700

    i updated my base module with json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,7 @@
 """ 
 this defines the base model calss
 """
-
+import json
 
 class Base:
     """
@@ -15,3 +15,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        this returns the JSON string representaion of list_dictioaries.
+        args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        to_json = json.dumps(list_dictionaries)
+
+        return to_json
