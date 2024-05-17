88a3a8485d95380b19bb1a4ab9d73fa8925ed895
bigcujo <princefrancisogbogu@gmail.com>
2024-05-17 07:07:40 -0700

    update my except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x):
+def safe_print_list(my_list=[], x=0):
 
     num = 0
 
@@ -11,5 +11,5 @@ def safe_print_list(my_list=[], x):
             num += 1
         except IndexError:
             break
-        print("")
-        return num
+    print("")
+    return num
