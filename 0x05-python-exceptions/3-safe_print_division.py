07f48d351ab88b269999d88df928edf2bf3143a6
bigcujo <princefrancisogbogu@gmail.com>
2024-05-17 08:13:30 -0700

    change division fuction

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-def safe_print_divison(a, b):
+def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
     except (ZeroDivisionError):
         result = None
     finally:
         print("inside result: {}".format(result))
-        return result
+     return result
