b2ac8ae04949877b36326993cad817814353b6ac
bigcujo <princefrancisogbogu@gmail.com>
2024-05-24 09:47:14 -0700

    edit full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,13 @@
 -- this script prints the full decription of the table  creted.
-SHOW CREATE TABLE 'first_table';
+SELECT
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM
+    information_schema.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE() AND
+    TABLE_NAME = 'first_table';
