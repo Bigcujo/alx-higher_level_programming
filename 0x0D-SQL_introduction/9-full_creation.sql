622f15484c16ddb365b75a1880020a6fb4c788ca
bigcujo <princefrancisogbogu@gmail.com>
2024-05-24 10:48:16 -0700

    edited syntax

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---this a second table and adds multiple rolls.
+-- this a second table and adds multiple rolls.
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
