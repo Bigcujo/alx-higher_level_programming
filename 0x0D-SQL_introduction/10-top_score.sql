7f97ed7857ddc1a42ed8c197c5dc298ceabaf442
bigcujo <princefrancisogbogu@gmail.com>
2024-05-24 10:41:29 -0700

    this lists all records of the second table in desecding order

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
--- this list all the records of the second table of the database in the server
--- it will be in decending order
-SELECT 'score', 'name'
-FROM 'second_table'
-ORDER BY 'score' DESC;
+-- this list all the records of the second table of the database in the server.
+-- it will be in decending order.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
