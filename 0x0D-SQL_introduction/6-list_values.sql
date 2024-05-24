3689a3b854f07a97738ba00c52b7333f900eb709
bigcujo <princefrancisogbogu@gmail.com>
2024-05-24 09:31:43 -0700

    edited the script on printing full description

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,4 @@
 -- this script list all the rows of the table from the database
-SELECT * FROM 'first_table';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_COMMENT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
