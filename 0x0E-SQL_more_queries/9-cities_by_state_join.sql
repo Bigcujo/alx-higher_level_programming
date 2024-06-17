14f631e25de5baa808298a6dc7b0b0666d8131e7
bigcujo <princefrancisogbogu@gmail.com>
2024-06-17 08:15:03 -0700

    myswql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- this will print all the cities in the databes hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name  FROM `cities` 
-JOIN `states` ON `cities.state_id` = `states.id`
+SELECT cities.id, cities.name, states.name  FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
