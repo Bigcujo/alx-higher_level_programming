05502f4c834f95175171372432493b09535c1e91
bigcujo <princefrancisogbogu@gmail.com>
2024-06-24 09:14:39 -0700

    added exection priviledge to a script

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const arrayString = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const i = 0; i <= 2; i++) {
+for (const i in arrayString) {
     console.log(arrayString[i]);
 }
