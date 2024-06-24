8b84400b18317dc7906068cf2a3b9404f0f1bf51
bigcujo <princefrancisogbogu@gmail.com>
2024-06-24 06:36:15 -0700

    first for loop

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+for (let i = 0; i <= 2; i++) {
+        if (i === 0) {
+                console.log('C is fun');
+        } else if (i === 1) {
+                console.log('Python is cool');
+        } else {
+                console.log('JavaScript is amazing');
+        }
+}
