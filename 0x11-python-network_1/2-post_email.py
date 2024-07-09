e89df1c247b733cc154d0152492f91a7f58f941e
bigcujo <princefrancisogbogu@gmail.com>
2024-07-09 08:17:53 -0700

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,8 @@ import urllib.request
 import urllib.parse
 if __name__ == '__main__':
     url = sys.argv[1]
-    value = {"email" : sys.argv[2]}
+    email_p = sys.ergv[2]
+    value = {"email" : email_p}
     data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
