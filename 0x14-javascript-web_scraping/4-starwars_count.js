bb80ad89ef3f2bf6ab2ac7cb48fc161bdbde14ab
Bigcujo <princefrancisogbogu>
2025-02-18 07:04:01 -0800

    finding number of time a an id shows

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body ) => {
+    if (error){
+        console.error(error);
+        return;
+    } 
+    try { 
+        const data = JSON.parse(body);
+
+        const count = data.results.filter( movie => 
+            movie.characters.some(character => character.includes('/18/'))
+            ).length;
+        console.log(count);
+    } catch (parseError) {
+        console.error('Error parsing JSON:', parseError);
+    }
+    }
+);
